f77b4fe04c5e49ae8ecc0f8a87c896c920dbc9e0
DuWenliang <739285514@qq.com>
2018-06-07 14:01:26 +0800

    对HTML的处理

---
+++ NSString/NSString+HtmlHandle.m
@@ -0,0 +1,26 @@
+//
+//  NSString+HtmlHandle.m
+//  TestByDuWenLiang
+//
+//  Created by ZZCN77 on 2018/6/5.
+//  Copyright © 2018年 ZZCN77. All rights reserved.
+//
+
+#import "NSString+HtmlHandle.h"
+
+@implementation NSString (HtmlHandle)
+
++ (NSString *)htmlEntityDecode:(NSString *)string
+{
+    string = [string stringByReplacingOccurrencesOfString:@"img src=&quot;" withString:@"img src=&quot;http://ld.tt.100help.net"];
+    string = [string stringByReplacingOccurrencesOfString:@"&quot;" withString:@"\""];
+    string = [string stringByReplacingOccurrencesOfString:@"&apos;" withString:@"'"];
+    string = [string stringByReplacingOccurrencesOfString:@"&lt;" withString:@"<"];
+    string = [string stringByReplacingOccurrencesOfString:@"&gt;" withString:@">"];
+    string = [string stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
+    //    string  = [string stringByReplacingOccurrencesOfString:@"KaiTi" withString:@"经典楷体简" options:NSRegularExpressionSearch range:NSMakeRange(0, string.length)];
+    
+    return string;
+}
+
+@end
